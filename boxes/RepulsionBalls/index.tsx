f929355ed0da90b5d8aaefb7ba7801ec79b3cb55
Linn Dahlgren <admin@opposite.biz>
2021-04-06 22:27:13 +0200

    Fixed delta timing

--- boxes/RepulsionBalls/index.tsx
+++ boxes/RepulsionBalls/index.tsx
@@ -4,9 +4,9 @@ import * as PIXI from 'pixi.js';
 import { Viewport } from 'stimbox/Components/ViewportContextProvider';
 
 const NUM_BALLS = 200;
-const DRAG = 0.7;
+const DRAG = 0.01;
 const MAX_VEL = 1000;
-const TIMESCALE = 0.3;
+const TIMESCALE = 1500;
 const FORCE_CONSTANT = 2;
 const WALL_CHARGE = 32 ** 2 * Math.PI;
 
@@ -84,7 +84,7 @@ export default function TestBox(): JSX.Element {
         requestAnimationFrame(animationFrame);
         return;
       }
-      const deltaT = t - lastT;
+      const deltaT = (t - lastT) / TIMESCALE;
       lastT = t;
 
       balls.forEach((ball, i) => {
@@ -99,14 +99,14 @@ export default function TestBox(): JSX.Element {
             Math.abs(ball2.x - ball.x),
             Math.min(ball.r, ball2.r),
           );
-          const scale = TIMESCALE / Math.max(diffY, diffX);
+          const scale = 1 / Math.max(diffY, diffX);
           const force =
             (ball.m * ball2.m * FORCE_CONSTANT) /
             Math.abs(diffX ** 2 + diffY ** 2);
           const xSign = Math.sign(ball2.x - ball.x) || 1;
           const ySign = Math.sign(ball2.y - ball.y) || 1;
-          const dvX = (xSign * (force * diffX * scale)) / deltaT;
-          const dvY = (ySign * (force * diffY * scale)) / deltaT;
+          const dvX = xSign * (force * diffX * scale) * deltaT;
+          const dvY = ySign * (force * diffY * scale) * deltaT;
           ball.vx -= dvX;
           ball.vy -= dvY;
           ball2.vx += dvX;
@@ -122,31 +122,29 @@ export default function TestBox(): JSX.Element {
             Math.abs(ball2.x - ball.x),
             Math.min(ball.r, ball2.r),
           );
-          const scale = TIMESCALE / Math.max(diffY, diffX);
+          const scale = 1 / Math.max(diffY, diffX);
           const force =
             (ball.m * ball2.m * FORCE_CONSTANT) /
             Math.abs(diffX ** 2 + diffY ** 2);
           const xSign = Math.sign(ball2.x - ball.x) || 1;
           const ySign = Math.sign(ball2.y - ball.y) || 1;
-          ball.vx -= (xSign * (force * diffX * scale)) / deltaT;
-          ball.vy -= (ySign * (force * diffY * scale)) / deltaT;
+          ball.vx -= xSign * (force * diffX * scale) * deltaT;
+          ball.vy -= ySign * (force * diffY * scale) * deltaT;
         }
         const { y } = ball;
         const invY = height - y;
         const { x } = ball;
         const invX = width - x;
         const forceX1 =
-          (ball.m * wallCharge * TIMESCALE * FORCE_CONSTANT) / Math.abs(x ** 2);
+          (ball.m * wallCharge * FORCE_CONSTANT) / Math.abs(x ** 2);
         const forceX2 =
-          (ball.m * wallCharge * TIMESCALE * FORCE_CONSTANT) /
-          Math.abs(invX ** 2);
+          (ball.m * wallCharge * FORCE_CONSTANT) / Math.abs(invX ** 2);
         const forceY1 =
-          (ball.m * wallCharge * TIMESCALE * FORCE_CONSTANT) / Math.abs(y ** 2);
+          (ball.m * wallCharge * FORCE_CONSTANT) / Math.abs(y ** 2);
         const forceY2 =
-          (ball.m * wallCharge * TIMESCALE * FORCE_CONSTANT) /
-          Math.abs(invY ** 2);
-        ball.vx += (forceX1 - forceX2) / deltaT;
-        ball.vy += (forceY1 - forceY2) / deltaT;
+          (ball.m * wallCharge * FORCE_CONSTANT) / Math.abs(invY ** 2);
+        ball.vx += (forceX1 - forceX2) * deltaT;
+        ball.vy += (forceY1 - forceY2) * deltaT;
         if (ball.x >= width - ball.r) {
           ball.x = width - ball.r;
           ball.vx = ball.vx > 0 ? -ball.vx : ball.vx;
@@ -167,18 +165,12 @@ export default function TestBox(): JSX.Element {
         }
         ball.vx = Math.max(Math.min(ball.vx, MAX_VEL), -MAX_VEL);
         ball.vy = Math.max(Math.min(ball.vy, MAX_VEL), -MAX_VEL);
-        ball.x += (ball.vx * TIMESCALE) / deltaT;
-        ball.y += (ball.vy * TIMESCALE) / deltaT;
+        ball.x += ball.vx * deltaT;
+        ball.y += ball.vy * deltaT;
         ball.gfx.x = ball.x;
-        ball.vx *= Math.max(
-          1 - (DRAG * ball.vx ** 2 * TIMESCALE) / (deltaT * MAX_VEL ** 2),
-          0,
-        );
+        ball.vx *= Math.max(1 - DRAG * deltaT, 0);
         ball.gfx.y = ball.y;
-        ball.vy *= Math.max(
-          1 - (DRAG * ball.vy ** 2 * TIMESCALE) / (deltaT * MAX_VEL ** 2),
-          0,
-        );
+        ball.vy *= Math.max(1 - DRAG * deltaT, 0);
         /* eslint-enable no-param-reassign */
       });
       requestAnimationFrame(animationFrame);
